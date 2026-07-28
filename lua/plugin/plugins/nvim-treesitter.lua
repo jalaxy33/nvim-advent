3525b2fc6e123fe4676ab08449983b5791f8627c
jalaxy33 <41473863+jalaxy33@users.noreply.github.com>
2026-07-28 09:22:57 +0800

    update nvim-treesitter

--- lua/plugin/plugins/nvim-treesitter.lua
+++ lua/plugin/plugins/nvim-treesitter.lua
@@ -38,4 +38,8 @@ local ensure_installed = {
   -- e.g. markdown, lua, vim... (run `:checkhealth vim.treesitter`)
 }
 
-require("nvim-treesitter").install(ensure_installed)
+NvimTS = require("nvim-treesitter")
+NvimTS.setup({
+  install_dir = vim.fn.stdpath('data') .. 'nvim-treesitter'
+})
+NvimTS.install(ensure_installed)
